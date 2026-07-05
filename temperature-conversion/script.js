9ab4a2ee5d0b8c01b4466a417713c4018bec82c0
Anushka Shakya <chiyaa2005@gmail.com>
2026-07-05 22:57:56 +0530

    temperature conversion

---
+++ temperature-conversion/script.js
@@ -0,0 +1,25 @@
+console.log("Script loaded");
+
+document.getElementById("submitBtn").onclick = function() {
+let temp;
+
+    if(document.getElementById("cBtn").checked) {
+        temp = document.getElementById("textBox").value;
+        temp = Number(temp);
+        temp = toCelsius(temp);
+        document.getElementById("tempLabel").innerHTML = temp + "°C";
+    } else if(document.getElementById("fBtn").checked) {
+        temp = document.getElementById("textBox").value;
+        temp = Number(temp);
+        temp = toFahrenheit(temp);
+        document.getElementById("tempLabel").innerHTML = temp + "°F";
+    } else {
+        document.getElementById("tempLabel").innerHTML = "Please select a conversion type.";
+    }
+}
+function toCelsius(temp) {
+    return (temp - 32) * 5/9;
+}
+function toFahrenheit(temp) {
+    return (temp * 9/5) + 32;
+}
